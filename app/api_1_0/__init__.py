308e35ec71de2d697a8d86b894776c2c87b70d33
Unknown <752481828@qq.com>
2018-02-23 14:41:54 +0800

    u

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -5,4 +5,4 @@ from flask import Blueprint
 
 api = Blueprint('api_1_0', __name__)
 
-from . import authentication, users, errors, hubs, timers
+from . import authentication, users, errors, hubs, timers, devices
