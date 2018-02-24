1ef38d879aeaa640ea2fb2ee7814b1f4202d78b4
Unknown <752481828@qq.com>
2018-02-24 16:16:32 +0800

    u

---
+++ app/api_1_0/qiniuyun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from flask import jsonify
+from qiniu import Auth
+
+from . import decorators
+
+# 需要填写你的 Access Key 和 Secret Key
+access_key = 'E-UOGVjHSKB59V8otdVUKHZtePBpW3jOJZH7SCbK'
+secret_key = 'cUfgICuOfZ5-WmOXg2O1h1TxQrdwUbHnn4XrS2bP'
+
+
+@decorators.route('/api/qiniu/<key>', methods=['GET'])
+def get_qiniu_token(key):
+    # 构建鉴权对象
+    q = Auth(access_key, secret_key)
+    # 要上传的空间
+    bucket_name = 'smarthub'
+    # 上传到七牛后保存的文件名key
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
+    print(token)
+    return jsonify({'msg': 'ok', 'result': token})
