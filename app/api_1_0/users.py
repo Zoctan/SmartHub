af1df8f358f35c229449688559b7ce1a21c6ed8c
Unknown <752481828@qq.com>
2018-02-24 13:05:46 +0800

    u

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -19,8 +19,6 @@ def login():
 @decorators.route('/api/users', methods=['GET'])
 def get_user_info():
     user = User.query.filter_by(id=g.current_user.id).first()
-    if not user:
-        return jsonify({'msg': 'no', 'error': '用户不存在'})
     return jsonify({'msg': 'ok', 'result': [user.to_json()]})
 
 
@@ -41,40 +39,34 @@ def add_user():
 
 @decorators.composed(decorators.route('/api/users/avatar', methods=['PUT']), decorators.json_required)
 def update_user_avatar():
-    user = User.query.filter_by(id=g.current_user.id).first()
-    if not user:
-        return jsonify({'msg': 'no', 'error': '用户不存在'})
     avatar = request.json.get('avatar')
     if not avatar:
         return jsonify({'msg': 'no', 'error': '图片链接不能为空'})
+    user = User.query.filter_by(id=g.current_user.id).first()
     user.avatar = avatar
     return jsonify({'msg': 'ok'})
 
 
 @decorators.composed(decorators.route('/api/users/password', methods=['PUT']), decorators.json_required)
 def update_user_password():
-    user = User.query.get(g.current_user.id)
-    if not user:
-        return jsonify({'msg': 'no', 'error': '用户不存在'})
     password = request.json.get('password')
     if not password:
         return jsonify({'msg': 'no', 'error': '密码不能为空'})
+    user = User.query.filter_by(id=g.current_user.id).first()
     user.password = password
     return jsonify({'msg': 'ok'})
 
 
 @decorators.composed(decorators.route('/api/users', methods=['PUT']), decorators.json_required)
 def update_user():
-    user = User.query.get(g.current_user.id)
-    if not user:
-        return jsonify({'msg': 'no', 'error': '用户不存在'})
     phone = request.json.get('phone')
     username = request.json.get('username')
-    if not phone and not username:
-        return jsonify({'msg': 'no', 'error': '参数不能为空'})
+    if not phone or not username:
+        return jsonify({'msg': 'no', 'error': '用户名或手机号不能为空'})
     user = User.query.filter_by(username=username).first()
     if user:
         return jsonify({'msg': 'no', 'error': '用户名已存在'})
+    user = User.query.filter_by(id=g.current_user.id).first()
     user.phone = phone
     user.username = username
     return jsonify({'msg': 'ok'})
