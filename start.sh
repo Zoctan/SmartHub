eb733bb8941c4b70e52481406b14e929d2f3c2cd
Unknown <752481828@qq.com>
2018-02-25 18:13:31 +0800

    u

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 git pull
 nohup ./manage.py runserver  2>/tmp/smarthub_err 1>/tmp/smarthub_out &
-cd ./app/api_1_0/ && nohup ./redis_timers.py 2>/tmp/redis_timers_err 1>/tmp/redis_timers_out &
+nohup ./redis_timers.py 2>/tmp/redis_timers_err 1>/tmp/redis_timers_out &
