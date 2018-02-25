0c0b1eecf4aabc507f0f50d2bc463d65c0807a0d
Unknown <752481828@qq.com>
2018-02-25 18:56:29 +0800

    u

--- restart.sh
+++ restart.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+prefix='smarthub_'
+
 function check_error() {
-  local files=$1'smarthub_*'
-  for file in `find $files `; do
-    local error=`grep -i error $file`
-    if [[ ! -z $error ]]; then
+  local files=$1${prefix}'*'
+  for file in $(find ${files}); do
+    local error=$(grep -i error ${file})
+    if [[ ! -z ${error} ]]; then
       echo "[!]$file error"
     fi
   done
 }
 
-ps -ef | grep '[.]/manage.py runserver' | awk '{print $2}' | xargs kill -9
-ps -ef | grep '[.]/run_redis_timers.py' | awk '{print $2}' | xargs kill -9
+ps -ef | grep '[.]/manage.py runserver' | awk '{print $2}' | xargs kill -9 &&
+ps -ef | grep '[.]/run_redis_timers.py' | awk '{print $2}' | xargs kill -9 &&
 
-rm /tmp/smarthub_*
+rm /tmp/${prefix}* &&
 
-git pull
-nohup ./manage.py runserver  2>/tmp/smarthub_err 1>/tmp/smarthub_out &
-nohup ./run_redis_timers.py 2>/tmp/smarthub_timers_err 1>/tmp/smarthub_timers_out &
+git pull &&
+nohup ./manage.py runserver  2>/tmp/${prefix}err 1>/tmp/${prefix}out &
+nohup ./run_redis_timers.py 2>/tmp/${prefix}timers_err 1>/tmp/${prefix}timers_out &
 
 check_error /tmp/
