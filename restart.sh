786a6682b997cde2179c69ba4a0f2aaa268f74fc
Unknown <752481828@qq.com>
2018-02-25 19:02:54 +0800

    u

--- restart.sh
+++ restart.sh
@@ -12,13 +12,34 @@ function check_error() {
   done
 }
 
+git pull &&
+
+echo '[*] git pull' &&
+
 ps -ef | grep '[.]/manage.py runserver' | awk '{print $2}' | xargs kill -9 ;
+
+echo '[*] kill api' &&
+
 ps -ef | grep '[.]/run_redis_timers.py' | awk '{print $2}' | xargs kill -9 ;
+
+echo '[*] kill redis timers'
 
 rm /tmp/${prefix}* &&
 
-git pull &&
+echo '[*] rm log' &&
+
 nohup ./manage.py runserver  2>/tmp/${prefix}err 1>/tmp/${prefix}out &
+
+echo '[*] start api' &&
+
+sleep 0.5 &&
+
 nohup ./run_redis_timers.py 2>/tmp/${prefix}timers_err 1>/tmp/${prefix}timers_out &
+
+echo '[*] start redis timers' &&
+
+sleep 0.5 &&
+
+echo '[*] start check error' &&
 
 check_error /tmp/
