917d1c69a972874e5cd700accd76ecd901c20d29
Unknown <752481828@qq.com>
2018-04-15 23:34:07 +0800

    u

--- app/api_1_0/result.py
+++ app/api_1_0/result.py
@@ -5,9 +5,12 @@ from flask import jsonify
 
 class Result:
     @staticmethod
-    def success(msg, result=''):
-        return jsonify({'msg': msg, 'error': 0, 'result': result}), {'Content-Type': 'application/json;charset=utf-8'}
+    def success(msg, result=None):
+        if result:
+            return jsonify({'msg': msg, 'error': 0, 'result': result}), {'Content-Type': 'application/json;charset=utf-8'}
+        else:
+            return jsonify({'msg': msg, 'error': 0}), {'Content-Type': 'application/json;charset=utf-8'}
 
     @staticmethod
-    def error(msg, result=''):
-        return jsonify({'msg': msg, 'error': 1, 'result': result}), {'Content-Type': 'application/json;charset=utf-8'}
+    def error(msg):
+        return jsonify({'msg': msg, 'error': 1}), {'Content-Type': 'application/json;charset=utf-8'}
